e587b4165ca8cd4c43d81f9fce8001e974c7b023
GaronSmith <wgaronsmith13@gmail.com>
2021-01-04 18:23:34 -0500

    moved login from app.js to routes/user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const csrf = require('csurf')
+const bcrypt = require('bcryptjs')
+const {User} = require('../db/models')
+
+
+
+const router = express.Router();
+const csrfProtection = csrf({cookie:true});
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/sign_up', csrfProtection, asyncHandler(async (req, res) => {
+  res.render('sign-up', { token: req.csrfToken() })
+}));
+
+router.get('/login', csrfProtection, asyncHandler(async (req, res) => {
+  res.render('login', { token: req.csrfToken() });
+}));
+
+router.post('/signup', csrfProtection, asyncHandler(async (req, res) => {
+  const { userName, firstName, lastName, email, bio, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ userName, firstName, lastName, email, bio, hashedPassword });
+  req.session.user = { id: user.id, userName: user.userName };
+  res.redirect('/');
+}));
+
+router.post('/login', csrfProtection, asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  const isPassword = await bcrypt.compare(password, user.hashedPassword.toString());
+
+  if (isPassword) {
+    req.session.user = { id: user.id, userName: user.userName }
+    console.log('Logged in.', req.session.user);
+    res.redirect('/');
+  } else {
+    console.log('Log in failure.');
+  }
+
+}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
