0bb5e51cdd9e9806eb5789f1cc96b332ca6fe7d6
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-04 16:13:58 -0600

    completed signup and login skeleton, authentication IS WORKING

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { sequelize } = require('./db/models');
+const { sequelize, User } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const csrf = require('csurf');
 const csrfProtection = csrf({cookie: true});
+const bcrypt = require('bcryptjs');
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
@@ -43,7 +44,34 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.get('/signup', csrfProtection, asyncHandler(async(req, res)=>{
-  res.render('signup', { token: req.csrfToken() } )
+  res.render('signup', { token: req.csrfToken() })
+}))
+
+app.get('/login', csrfProtection, asyncHandler(async(req, res) => {
+  res.render('login', { token: req.csrfToken() });
+}))
+
+app.post('/signup', csrfProtection, asyncHandler(async(req, res) => {
+  const { userName, firstName, lastName, email, bio, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ userName, firstName, lastName, email, bio, hashedPassword });
+  req.session.user = { id: user.id, userName: user.userName };
+  res.redirect('/');
+}))
+
+app.post('/login', csrfProtection, asyncHandler(async(req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email }});
+  const isPassword = await bcrypt.compare(password, user.hashedPassword.toString());
+  
+  if (isPassword) {
+    req.session.user = { id: user.id, userName: user.userName }
+    console.log('Logged in.', req.session.user);
+    res.redirect('/');
+  } else {
+    console.log('Log in failure.');
+  }
+
 }))
 
 // catch 404 and forward to error handler
