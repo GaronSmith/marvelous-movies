e2b2f0ca4a3b370f7d3a744fa017965cf7b7bf13
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-05 16:04:44 -0600

    movie profile page rendering

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
+
 const csrf = require('csurf');
 const csrfProtection = csrf({cookie: true});
 const bcrypt = require('bcryptjs');
@@ -42,6 +44,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/movies.js
+++ routes/movies.js
@@ -4,19 +4,20 @@ const csrf = require('csurf');
 const router = express.Router();
 const csrfProtection = csrf({ cookie: true });
 
-const { Movie } = require('../db/models');
+const db = require('../db/models');
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 // Movie Profile Page - single movie - retrieve/GET by ID
 
-router.get('/movies/:id(\\d+)', csrfProtection, asyncHandler(async(req, res) => {
+router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
     // Do we need csrfProtection on this GET route?
     // single movie info pulled from database
     const movieId = parseInt(req.params.id, 10);
+    console.log(movieId);
     // include Ratings, Reviews
-    const movie = await Movie.findByPk(movieId);
+    const movie = await db.Movie.findByPk(movieId);
     res.render('movie-profile', { title: 'Movie Profile', movie });
 }));
 
-
+module.exports = router;
