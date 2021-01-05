f360355bbb9d650aa5855c2d61be978fc22ace57
Benjamin Lavian <benylavian@gmail.com>
2021-01-05 16:54:33 -0500

    user profile basically done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = (handler) => (req,res,next) => handler(req,res,next).catch(next)
+const db = require('../db/models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond ith a resource');
-});
+
+/* GET users profile. */
+router.get('/:id', asyncHandler(async(req, res)=> {
+const currentUser = req.session.auth.userId
+const users = await db.User.findByPk(currentUser,{
+  include:{
+    model:db.Movie
+  }
+})
+res.json()
+res.render('profile',users)
+}))
 
 module.exports = router;
