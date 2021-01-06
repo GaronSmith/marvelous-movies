e64cc6cb84644e0a93680432786142de24c9d741
GaronSmith <wgaronsmith13@gmail.com>
2021-01-06 18:58:15 -0500

    revert app

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const searchRouter = require('./routes/search')
 const csrf = require('csurf');
 const csrfProtection = csrf({cookie: true});
 const bcrypt = require('bcryptjs');
@@ -46,7 +45,6 @@ app.use(
   app.use(restoreUser)
   app.use('/', indexRouter);
   app.use('/users', usersRouter);
-  app.use('/search', searchRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
