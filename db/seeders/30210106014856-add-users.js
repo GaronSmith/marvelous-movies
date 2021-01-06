f2b1be354c0708851b8e1799bc802bbb16befc2c
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-05 19:36:13 -0700

    users seed file

---
+++ db/seeders/30210106014856-add-users.js
@@ -0,0 +1,37 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = []
+
+    for( let i = 0; i < 50; i++){
+      const newUser = {
+        userName: faker.internet.userName(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        email: faker.internet.email(),
+        bio: faker.lorem.sentence(),
+        hashedPassword: await bcrypt.hash(`Password${i}`, 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+      users.push(newUser)
+    }
+    
+      return queryInterface.bulkInsert('Users', users , {});
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
