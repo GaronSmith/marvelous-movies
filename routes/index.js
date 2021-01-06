af5f86e6a528e3c9c12ee4578cae3804da0adb9d
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-06 15:00:24 -0700

    working on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const { User, Movie, Review } = require('../db/models')
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next)
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
-});
+router.get('/', asyncHandler(async(req, res, next) => {
+  const topMovies = await Movie.findAll({where: Movie.voteRating > 4, limit: 5});
+  const recentReviews = await Review.findAll({ include: Movie, order: Review.updatedAt, limit: 5})
+  
+  res.render('index', {topMovies, recentReviews, title: 'Welcome to Marvelous Movies!' });
+}));
 
 module.exports = router;
