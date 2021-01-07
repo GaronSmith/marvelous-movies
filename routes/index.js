248eee7d29b7c7c51c927c224bb5c3e5a08b4853
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-07 15:49:18 -0700

    functional and kinda cute homepage for not logged in user with the ability to click on links for best films in a genre

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const csrf = require('csurf');
-const csrfProtection = csrf({cookie: true})
-const { Sequelize } = require('sequelize')
-const Op = Sequelize.Op
+const csrfProtection = csrf({cookie: true});
+const { Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
 
-const { User, Movie, Review, sequelize } = require('../db/models')
-const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next)
+const { User, Movie, Review, sequelize } = require('../db/models');
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
-/* GET home page. */
 
 router.get('/', csrfProtection, asyncHandler(async(req, res, next) => {
   const topMovies = await Movie.findAll({where: { voteRating:{ [Op.gt]: 8 }, voteCount: { [Op.gt]: 2000 } }, order: [['voteRating', 'DESC']], limit: 5});
-  const recentReviews = await Review.findAll({ include: [Movie, User], order: [['updatedAt', 'DESC']], limit: 5})
-  
-  res.render('index', { token: req.csrfToken(), topMovies, recentReviews, title: 'Welcome to Marvelous Movies!' });
+  const recentReviews = await Review.findAll({ include: [Movie, User], order: [['updatedAt', 'DESC']], limit: 5});
+  function randomNum(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  const randomBest = randomNum(880);
+  const bestFilm = await Movie.findByPk(randomBest);
+  res.render('index', { token: req.csrfToken(), topMovies, recentReviews, bestFilm, title: 'Welcome to Marvelous Movies!' });
 }));
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -1,22 +1,20 @@
-var express = require('express');
-var router = express.Router();
-const csrf = require('csurf');
-const csrfProtection = csrf({ cookie: true });
-const { Movie }= require('../db/models')
+const express = require('express');
+const router = express.Router();
+const { Movie }= require('../db/models');
 const { Op } = require('sequelize');
 
 const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
 function toProperCase(word) {
-    let letters = word.split('')
-    letters[0].toUpperCase()
-    return letters.join('')
+    let letters = word.split('');
+    letters[0].toUpperCase();
+    return letters.join('');
 }
 
 router.get('/genre/:id', asyncHandler(async(req, res)=>{
-    const genre = toProperCase(req.params.id)
-    const topMovies = await Movie.findAll({ where: {genre}, limit: 25, order: [['voteRating', 'DESC']] })
-    res.render('top-movies', { topMovies, genre })
-}))
+    const genre = toProperCase(req.params.id);
+    const topMovies = await Movie.findAll({ where: {genre}, limit: 25, order: [['voteRating', 'DESC']] });
+    res.render('top-movies', { topMovies, genre });
+}));
 
 module.exports = router;
