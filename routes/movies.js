0dfbaf727ff081e522b545530ae15acba5b4ed02
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-01-06 19:00:33 -0700

    working on homepage and top movies

---
+++ routes/movies.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+const { Movie }= require('../db/models')
+const { Op } = require('sequelize');
+
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
+
+function toProperCase(word) {
+    let letters = word.split('')
+    letters[0].toUpperCase()
+    return letters.join('')
+}
+
+router.get('/genre/:id', asyncHandler(async(req, res)=>{
+    const genre = toProperCase(req.params.id)
+    const topMovies = await Movie.findAll({ where: {genre}, limit: 25, order: [['voteRating', 'ASC']] })
+    res.render('top-movies', { topMovies, title: 'welcome to the top movies page' })
+}))
+
+module.exports = router;
