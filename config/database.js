bd4408e3a537da2681edf60a007103db40caf54e
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-07 11:34:31 -0600

    revert database

--- config/database.js
+++ config/database.js
@@ -11,9 +11,4 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
-  }
 };
