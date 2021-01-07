9151198b2121bc5470357600754a673c126767ba
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-07 15:37:45 -0600

    made code review changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const reviewsRouter = require('./routes/reviews');
+// const moviesRouter = require('./routes/movies');
 const csrf = require('csurf');
 const csrfProtection = csrf({cookie: true});
 const bcrypt = require('bcryptjs');
@@ -48,6 +49,7 @@ app.use(
   app.use('/', indexRouter);
   app.use('/users', usersRouter);
   app.use('/reviews', reviewsRouter);
+  // app.use('/movies', moviesRouter);
   
 
 
--- routes/movies.js
+++ routes/movies.js
@@ -11,12 +11,8 @@ const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).ca
 router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
     const movieId = parseInt(req.params.id, 10);
     const movie = await db.Movie.findByPk(movieId);
-    // const year = movie.releaseDate.toString().split(' ')[3];
     const year = movie.releaseDate.getFullYear();
     res.render('movie-profile', { title: 'Movie Profile', movie, year });
 }));
-
-// TODO: Status Route
-    // add button with drop down functionality (pug,css)
 
 module.exports = router;
