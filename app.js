aa31e1db39d3b35eb813933a4d400b4ce766dbca
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2021-01-06 18:28:06 -0600

    revised css to flexbox, added styling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
 
 const csrf = require('csurf');
 const csrfProtection = csrf({cookie: true});
@@ -46,6 +47,7 @@ app.use(
   app.use(restoreUser)
   app.use('/', indexRouter);
   app.use('/users', usersRouter);
+  app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
