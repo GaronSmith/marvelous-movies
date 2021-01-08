744f3851fca82f893a84c2491b2cf59fea752360
GaronSmith <wgaronsmith13@gmail.com>
2021-01-08 14:16:03 -0500

    added feed logic

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -3,6 +3,10 @@ const renderFeed = json => {
     const feedAnchor = document.getElementById('feed-results')
 }
 
+const getFeed = async () => {
+    const res = await fetch()
+}
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   
